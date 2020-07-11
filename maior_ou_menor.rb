1ff69b45d23e1a10fb66cc39c11a9925983a4303
João Pedro <jotapebclh@gmail.com>
2020-07-10 22:13:52 -0300

    If,else,for

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -11,16 +11,35 @@ puts "Escolhendo um número secreto entre 0 e 200"
 numero_secreto = 175
 
 puts "Número escolhido, que tal adivinha-lo?"
+
+tentativas = 3
+
+for tentativas in 1..3 do
+    
+    puts "\n"
+    puts "Tentativa " + tentativas.to_s
+    puts "Entre com o seu número"
+    chute = gets
+    
+    puts "\n"
+    puts "Será que acertou? Você chutou " + chute
+    puts "\n"
+    
+    acertou = numero_secreto == chute.to_i
+    
+    if acertou
+        puts "Acertou!"
+    else
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior."
+        else
+            puts "O número secreto é menor."
+        end
+    end
+    
+end
+
 puts "\n"
-
-puts "Tentativa 1"
-puts "Entre com o seu número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-
-puts chute.to_i == 175
-
-
-
-
+puts "\n"
 
