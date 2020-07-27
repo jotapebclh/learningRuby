22ce8306c33dd36bf18e5ea9dcd723e05700eea9
João Pedro <jotapebclh@gmail.com>
2020-07-27 20:34:40 -0300

    Refactoring

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,48 +1,68 @@
-puts "Bem vindo ao Jogo da Adivinhação."
-puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n"
-
-puts "Começaremos o jogo para você, " + nome
-puts "\n"
-
-puts "Escolhendo um número secreto entre 0 e 200"
-numero_secreto = 175
-
-puts "Número escolhido, que tal adivinha-lo?"
-
-limite_de_tentativas = 5
-
-for tentativas in 1..limite_de_tentativas
-    
+def da_boas_vindas()
+    puts "Bem vindo ao Jogo da Adivinhação."
+    puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+    puts "Qual é o seu nome?"
+    nome = gets
     puts "\n"
-    puts "Tentativa " + tentativas.to_s + " de " + limite_de_tentativas.to_s
+
+    puts "Começaremos o jogo para você, " + nome
+    puts "\n"
+end
+
+def sorteia_numero_secreto()
+    puts "Escolhendo um número secreto entre 0 e 200"
+    sorteado = 175
+
+    puts "Número escolhido, que tal adivinha-lo?"
+
+    sorteado
+end
+
+def pede_numero(tentativa, limite_de_tentativas)
+    puts "\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     puts "Entre com o seu número"
     chute = gets
     
     puts "\n"
     puts "Será que acertou? Você chutou " + chute
     puts "\n"
-    
-    acertou = numero_secreto == chute.to_i
-    
+
+    chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute    
     if acertou
         puts "Acertou!"
-        break
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior."
-            puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-            puts "\n"
-        else
-            puts "O número secreto é menor."
-            puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-            puts "\n"
-        end
+
+        return true
     end
-    
+
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior."
+        puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+        puts "\n"
+    else
+        puts "O número secreto é menor."
+        puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+        puts "\n"
+    end
+    return false
+end
+
+da_boas_vindas()
+
+numero_secreto = sorteia_numero_secreto()
+
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+    chute = pede_numero(tentativa, limite_de_tentativas)
+
+    if verifica_se_acertou(numero_secreto, chute)
+        break
+    end
 end
 
 puts "\n"
